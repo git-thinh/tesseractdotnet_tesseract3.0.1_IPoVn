9231edd6daf7795b1b2cd31c6024f48897e3807d
nguyenvanthinh <nguyenvanthinh@f88.vn>
2019-12-31 16:31:59 +0700

    ok

--- tesseractdotnetwrapper_r590/IPoVn.OCRer/Program.cs
+++ tesseractdotnetwrapper_r590/IPoVn.OCRer/Program.cs
@@ -1,6 +1,8 @@
-﻿using OCR.TesseractWrapper;
+﻿using IPoVn.IPCore;
+using OCR.TesseractWrapper;
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Reflection;
 
@@ -16,16 +18,17 @@ namespace IPoVn.OCRer
 
     class Program
     {
-        //const string path_tessdata = @"C:\IPoVn\Test\tessdata\";
-        const string path_tessdata = @"tessdata\";
-
-        const string OutputFolder = "_";
+        //const string TessdataFolder = @"C:\IPoVn\Test\tessdata\";
+        const string TessdataFolder = @"tessdata\";
         static string[] Images = new string[] {
             @"phototest.tif",
             @"eurotext.tif",
             @"sample3.png",
             @"sample4.jpg"
         };
+
+        const string InputFolder = "";
+        const string OutputFolder = "_";
 
         static void Simple1_Recognize()
         {
@@ -41,7 +44,7 @@ namespace IPoVn.OCRer
                     //for (int i = 3; i < 4; i++)
                     {
                         oem = i;
-                        bool ok = processor.Init(path_tessdata, "eng", i); 
+                        bool ok = processor.Init(TessdataFolder, "eng", i);
                         if (ok)
                         {
                             string text = "";
@@ -67,7 +70,6 @@ namespace IPoVn.OCRer
                 }
             }
         }
-
         static void Simple1_AnalyseLayout()
         {
             int n_images = Images.Length;
@@ -133,8 +135,135 @@ namespace IPoVn.OCRer
             }
         }
 
-        static void Simple2_Recognize() { }
-        static void Simple2_AnalyseLayout() { }
+        static void Simple2_Recognize()
+        {
+            int n_images = Images.Length;
+            int i_image = n_images - 1;
+            //i_image = 0;
+            string fileName = Images[i_image];
+
+            string imageFile = Path.Combine(InputFolder, fileName);
+
+            string language = "eng";
+            int oem = (int)eOcrEngineMode.OEM_DEFAULT;
+
+            using (TesseractProcessor processor = new TesseractProcessor())
+            {
+                using (Bitmap bmp = Bitmap.FromFile(imageFile) as Bitmap)
+                {
+                    using (GreyImage greyImage = GreyImage.FromImage(bmp))
+                    {
+
+                        ImageThresholder thresholder = new AdaptiveThresholder();
+                        using (BinaryImage binImage = thresholder.Threshold(greyImage))
+                        {
+                            DateTime started = DateTime.Now;
+                            DateTime ended = DateTime.Now;
+
+                            int i = 3;
+                            //for (i = 0; i < 4; i++)
+                            //for (i = 3; i < 4; i++)
+                            {
+                                oem = i;
+                                processor.Init(TessdataFolder, language, oem);
+
+                                string text = "";
+                                unsafe
+                                {
+                                    started = DateTime.Now;
+
+                                    text = processor.RecognizeBinaryImage(
+                                        binImage.BinaryData, greyImage.Width, greyImage.Height);
+
+                                    ended = DateTime.Now;
+
+                                    Console.WriteLine("Duration recognition: {0} ms\n\n", (ended - started).TotalMilliseconds);
+                                }
+
+                                Console.WriteLine(
+                                    string.Format("RecognizeMode: {1}\nRecognized Text:\n{0}\n++++++++++++++++++++++++++++++++\n", text, ((eOcrEngineMode)oem).ToString()));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void Simple2_AnalyseLayout()
+        {
+            int n_images = Images.Length;
+            int i_image = 0;
+            for (; i_image < n_images; i_image++)
+            {
+                string fileName = Images[i_image];
+
+                Console.WriteLine("{0} Image: {1}", i_image, fileName);
+
+                string imageFile = Path.Combine(InputFolder, fileName);
+
+                string name = Path.GetFileNameWithoutExtension(imageFile);
+
+                string outFile = Path.Combine(OutputFolder, string.Format("Simple2_{0}_layout.bmp", name));
+                string outFile2 = Path.Combine(OutputFolder, string.Format("Simple2_{0}_grey.bmp", name));
+                string outFile3 = Path.Combine(OutputFolder, string.Format("Simple2_{0}_bin.bmp", name));
+
+                using (TesseractProcessor processor = new TesseractProcessor())
+                {
+                    processor.InitForAnalysePage();
+                    //processor.SetPageSegMode(ePageSegMode.PSM_AUTO);
+
+                    using (Bitmap bmp = Bitmap.FromFile(imageFile) as Bitmap)
+                    {
+                        using (GreyImage greyImage = GreyImage.FromImage(bmp))
+                        {
+                            greyImage.Save(ImageFormat.Bmp, outFile2);
+
+                            ImageThresholder thresholder = new AdaptiveThresholder();
+                            using (BinaryImage binImage = thresholder.Threshold(greyImage))
+                            {
+                                binImage.Save(ImageFormat.Bmp, outFile3);
+
+                                DateTime started = DateTime.Now;
+                                DateTime ended = DateTime.Now;
+
+                                DocumentLayout doc = null;
+
+                                unsafe
+                                {
+                                    started = DateTime.Now;
+
+                                    doc = processor.AnalyseLayoutBinaryImage(
+                                        binImage.BinaryData, greyImage.Width, greyImage.Height);
+
+                                    ended = DateTime.Now;
+
+                                    Console.WriteLine("Duration AnalyseLayout: {0} ms", (ended - started).TotalMilliseconds);
+                                }
+                                Console.WriteLine(doc.ToString());
+
+                                using (Image tmp = new Bitmap(bmp.Width, bmp.Height)) // prevents one-byte index format
+                                {
+                                    using (Graphics grph = Graphics.FromImage(tmp))
+                                    {
+                                        Rectangle rect = new Rectangle(0, 0, tmp.Width, tmp.Height);
+
+                                        grph.DrawImage(bmp, rect, rect, GraphicsUnit.Pixel);
+
+                                        grph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                                        foreach (Block block in doc.Blocks)
+                                        {
+                                            Render.DrawBlock(grph, block);
+                                        }
+                                    }
+
+                                    tmp.Save(outFile);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         static void Simple3_Recognize()
         {
@@ -144,10 +273,10 @@ namespace IPoVn.OCRer
         static void Main(string[] args)
         {
             //Simple1_Recognize();
-            Simple1_AnalyseLayout();
+            //Simple1_AnalyseLayout();
 
             //Simple2_Recognize();
-            //Simple2_AnalyseLayout();
+            Simple2_AnalyseLayout();
 
             //Simple3_Recognize();
 
